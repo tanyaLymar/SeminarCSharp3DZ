0bd7a05d1986234a5d1e742d159b1845a804d47d
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-25 22:55:11 +0300

    Task1 solved

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Найти кубы чисел от 1 до N
+Console.Write("Определим случайным образом число N: ");
+ int n = new Random().Next(1,10);
+ Console.WriteLine(n);
+ for (int i = 1;i<=n; i++)
+    {
+    Console.WriteLine(i + " - " + i * i* i);
+    }
+
