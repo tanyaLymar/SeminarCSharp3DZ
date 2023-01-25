436eedc59696bea4d5bd939dda28ea651d9cbf21
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-25 23:24:40 +0300

    Task3 solved

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Возведите число А в натуральную степень B используя цикл
+Console.Write("Определим случайным образом число A: ");
+ int a = new Random().Next(1,10);
+ Console.WriteLine(a);
+
+ Console.Write("Определим случайным образом степень: ");
+ int n = new Random().Next(1,10);
+ Console.WriteLine(n); 
+
+int result = 1;
+
+for (int i = 1; i <= n; i++)
+{
+ result *= a;
+}
+
+Console.WriteLine(result);
+
