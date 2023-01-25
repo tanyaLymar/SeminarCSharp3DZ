7d80d9084a5fb7e6a6611d81661681a70c9154bd
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-25 23:02:02 +0300

    Task1 solved

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Определим случайным образом число A: ");
+ int a = new Random().Next(1,10);
+ Console.WriteLine(a);
+ int result = 0;
+ for (int i = 1;i<=a; i++)
+    {
+    result = result + i;
+    }
+Console.WriteLine(result);
