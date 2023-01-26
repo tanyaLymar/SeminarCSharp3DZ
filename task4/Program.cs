9a59fc90d1f8bdb3e6c05e2d2aeaf01651317e5c
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-26 20:22:38 +0300

    Task4 solved

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Write("Введите число: ");
+ int n = int.Parse(Console.ReadLine() ?? "0");
+
+int mod = 0;
+int count = 0;
+while(n > 0)
+{
+    mod = mod + n%10;
+    n = n/10;
+    count +=1;
+}
+Console. WriteLine(count);
+
+
+
