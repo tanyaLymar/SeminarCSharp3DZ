6759c2157d83ddb575546312ef0bc26340e8319b
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-26 20:47:03 +0300

    Task6 solved

---
+++ task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите число: ");
+ int n = int.Parse(Console.ReadLine() ?? "0");
+
+ for (int i = 0; i <= n; i+=2)
+ {
+  Console.WriteLine(i + " - " + i * i* i);  
+ }
