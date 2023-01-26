7a234d9f54153068d47616fdcd2de6c70f791009
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-26 21:43:17 +0300

    Task7 solved

---
+++ task7/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void FillMas (int[]array)
+{
+   for (int i = 0; i < array.Length; i++)
+    {
+        int value = new Random().Next(2);
+        array[i] =value;
+    }
+}
+
+void PrintMas(int [] array)
+{
+    foreach(int item in array)
+    {
+        Console.Write(item + ", ");
+    }
+    Console.WriteLine();
+}
+    
+Console.Write("Введите размерность массива: ");    
+int n = int.Parse(Console.ReadLine() ?? "0");
+int [] array = new int[n];
+FillMas(array);
+PrintMas(array);
