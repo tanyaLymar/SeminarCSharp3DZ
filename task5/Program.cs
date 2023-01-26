7e7773d0d3a0a01a90a9543aa34eee0317d59f91
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-26 20:36:24 +0300

    Task5 solved

---
+++ task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число: ");
+ int n = int.Parse(Console.ReadLine() ?? "0");
+ int factorial = 1;
+
+ for (int i = 1; i <=n; i++)
+ {
+    factorial = factorial * i;
+ }
+Console.WriteLine(factorial);
